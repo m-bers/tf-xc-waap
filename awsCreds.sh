7df21141bd11bc99ddada386ea2b1887d93c169b
Josh Chambers <j.chambers@f5.com>
2022-06-29 17:56:27 +0000

    modify .gitignore

--- awsCreds.sh
+++ awsCreds.sh
@@ -1,3 +1,5 @@
+# Script to retrieve ephemeral AWS account credentials from UDF API
+
 printf "[default]\naws_access_key_id=" > ~/.aws/credentials
 curl -s 10.1.1.1/cloudAccounts | jq '.cloudAccounts[0]' | jq -r ' .apiKey' >> ~/.aws/credentials
 printf "aws_secret_access_key=" >> ~/.aws/credentials
