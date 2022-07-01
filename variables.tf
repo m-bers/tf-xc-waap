6fc56735a19c6af122145e8eba6db772da151e51
Josh Chambers <j.chambers@f5.com>
2022-07-01 02:35:22 +0000

    strip variable defaults

--- variables.tf
+++ variables.tf
@@ -1,21 +1,9 @@
-variable "private_key_path" {
-  default = "/home/ubuntu/.ssh/id_rsa"
-}
-variable "public_key_path" {
-  default = "/home/ubuntu/.ssh/id_rsa.pub"
-}
-variable "ec2_user" {
-  default = "ubuntu"
-}
-
+variable "private_key_path" {}
+variable "public_key_path" {}
+variable "ec2_user" {}
 variable "xc_password" {}
-
 variable "xc_namespace" {}
-
 variable "xc_tenant" {}
-
 variable "xc_p12_path" {}
-
 variable "xc_api_url" {}
-
 variable "swaggerfile_location" {}
